f3d70ed721db4f8232730556badaa52870752f2a
olzhasamir <49907256+olzhasamir@users.noreply.github.com>
2020-02-18 17:09:50 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import numpy as np
+count = 0                          
+number = np.random.randint(1,100)   
+def score_game(game):
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 100, size=(1000))
+    for number in random_array:
+        count_ls.append(game(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return(score)
+    def game333(number):
+    print(number)
+    count = 0
+    predict = np.random.randint(1,100)
+    max_value = 100
+    min_value = 1
+    while number != predict:
+        count+=1
+        if predict > number: 
+            max_value = predict
+        else: 
+            min_value = predict
+        predict = min_value + int((max_value - min_value)/2)
+        print(count, predict)
+    return(count) 
+    score_game(game333)
